e5763067a099569aecc5790fe3016b2eb713f28d
Shantanu Tyagi <shantanutyagi67@gmail.com>
2021-01-06 05:35:06 +0530

    basic implementation done

---
+++ info.py
@@ -0,0 +1,18 @@
+# Import modules for CGI handling 
+import cgi, cgitb 
+
+# Create instance of FieldStorage 
+form = cgi.FieldStorage() 
+
+# Get data from fields
+first_name = form.getvalue('url')
+last_name  = form.getvalue('cost')
+print ("Content-type:text/html\r\n\r\n")
+print ("<html>")
+print ("<head>")
+print ("<title>Hello - Second CGI Program</title>")
+print ("</head>")
+print ("<body>")
+print ("<h2>Hello %s %s</h2>" % (first_name, last_name))
+print ("</body>")
+print ("</html>")
